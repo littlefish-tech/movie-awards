8113030b7e5624c2cf4774fdbb545367b468b204
Yimeng Yu <yimeng.yu@gmail.com>
2021-05-09 13:23:27 -0700

    scroll to top button work

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../style/scrollToTop.css";
+import top from "../asset/images/top.png";
 
 export default function ScrollToTop() {
-  return <div>scroll to top</div>;
+  // scroll to top idea give credit to https://www.coderomeos.org/scroll-to-top-of-the-page-a-simple-react-component
+  const [visible, setVisible] = useState<boolean>(false);
+
+  function toggleVisibility() {
+    if (window.pageYOffset > 300) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <div className="scrollToTop">
+      {visible && (
+        <div onClick={scrollToTop}>
+          <img src={top} alt="Go to top" />
+        </div>
+      )}
+    </div>
+  );
 }
